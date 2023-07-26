0f0dfe10a58dd02cdf71532be9f42e9bfdfe6539
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-26 23:28:07 +0300

    Create new commit 26.07.2023

---
+++ JUnitTest/test/com/howtodoinjava/junit5/FirstTest.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+
+	@Test
+	void test() {
+		System.out.println("BMW");
+	}
+
+}
