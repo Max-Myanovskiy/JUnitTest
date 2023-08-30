9319c8480334efeed0df4d5e723d323dc5b26aaf
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-08-30 23:16:14 +0300

    New file 30.08.2023

---
+++ JUnitTest/src/test/java/com/howtodoinjava/junit5/FirstTest2.java
@@ -0,0 +1,14 @@
+package test.java.com.howtodoinjava.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstTest2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
